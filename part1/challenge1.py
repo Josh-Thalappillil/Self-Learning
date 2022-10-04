22a0838ad09ea78091a76dd94f68cc3d2bd49492
Joshi <joshi@Nigrutos-Macbook.local>
2022-10-05 09:04:32 +1100

    Challenge 1 completed

--- part1/challenge1.py
+++ part1/challenge1.py
@@ -2,3 +2,18 @@
 #Variable is age
 #variable is assigned to an integer
 #prints strings depending on integer age
+
+def age():
+    age = int(input("Please enter your age: "))
+    
+    if age <= 12:
+        print("You are a child.")
+    
+    elif age >= 13 and age <= 19:
+        print("You are a teenager.")
+    
+    elif age >= 20:
+        print("You are an adult.")
+
+
+age()
