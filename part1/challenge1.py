1cfb5aa30dd0818374b67972929c7e27169f855a
Joshi <joshi@Nigrutos-Macbook.local>
2022-10-04 12:12:30 +1100

    Added new challenge

--- part1/challenge1.py
+++ part1/challenge1.py
@@ -1,9 +1,4 @@
-from re import A
-
-
-x = 10 
-y = 10 
-z=x+y 
-a=x-y
-print(z)
-print(a)
+#Write a program with a variable called age assigned to an integer that prints different strings depending on what integer age is.
+#Variable is age
+#variable is assigned to an integer
+#prints strings depending on integer age
