d3f895bc8a602b351c038b05589fd13a71f51451
Joshi <joshi@Nigrutos-Macbook.local>
2022-10-05 14:44:27 +1100

    Updated learning1.py

--- part1/learning1.py
+++ part1/learning1.py
@@ -28,3 +28,62 @@ def f(x, y, z):
 
 result = f(1, 2, 3)
 print(result)
+
+#parameters that can pass
+
+def f(x=10):
+    if x == 10:
+        print("x is ten")
+    else:
+        print("x is not 10")
+
+default = f()
+pass_in = f(2)
+
+
+
+#Nested functions
+
+def nest():
+    print("Inner Function!")
+    def x():
+        print("Nested Function")
+    x()
+
+
+#variables-learning how to interact with global varibales
+x = 100
+def variable():
+    global x
+    x += 2
+    print(x)
+
+
+#built in functions
+
+#length
+def len():
+    print(len("Flabberghasted"))
+
+#data types
+def datatypes():
+    print(type("Hello World"))
+    print(type(100))
+    print(type(1.0))
+
+    print(str(100))
+    print(int("125"))
+    print(float(100))
+
+#inputs
+def agetest():
+
+    age = input("How old are you? ")
+    age = int(age)
+    if age <= 12:
+        print("You are a child.")
+    elif age >= 13 and age <= 19:
+        print("You are a teenager.")
+    elif age >=20:
+        print("You are an adult.")
+
