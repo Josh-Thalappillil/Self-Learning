f71d565660afc56129f45055a1b75797f2f5044a
Josh-Thalappillil <jthalappillil7@gmail.com>
2022-10-13 12:42:32 +1100

    Completed new challenge and updated learning.

---
+++ part1/challengechp7.py
@@ -0,0 +1,21 @@
+#Write a program that has an infinite loop (with q to quit) 
+#and each time through the loop, it asks the user to guess a number 
+#and tells them whether their guess was right or wrong.
+
+#boolean, Running = True
+#declare guess number (random)
+#while loop for running, if user input == guess number, running = false.
+
+import random
+
+guess_number = random.randint(1, 10)
+running = True
+
+while running:
+    guess = int(input("Enter your guess: "))
+    if guess == guess_number:
+        running = False
+        print("Congratulations, you chose the correct number.")
+    
+    else:
+        print("Guess again.", str(guess_number))
--- part1/learning2.py
+++ part1/learning2.py
@@ -1,5 +1,6 @@
 # Continuing from chapter 5 learning about lists and tuples
 
+from argparse import ONE_OR_MORE
 from xmlrpc.client import boolean
 
 
@@ -21,5 +22,42 @@ def dictionary():
     my_dictionary["Bill Gates"] = "rich"
     print(my_dictionary["Bill Gates"])
 
-dictionary()
+
+
+#Chapter 6 String Manipulation
+def upper():
+    print("""line one
+        line two
+        line three
+    """ .capitalize())
+
+
+def chapter6():
+
+    my_dict = {"Sports car":"Skyline", "Modifications":"turbo"}
+    for key in my_dict:
+        print(key)
+
+    x = 0
+    while x < 10:
+        print(x)
+        x += 1
+    print("Happy New Year")
+
+def breaks():
+    questions = ["What is your name?", "What is your favorite color?", "What is your quest?"]
+    n = 0
+    while True:
+        print("Type q to quit" + str(n)) 
+        answer = input(questions[n]) 
+        if answer == "q":
+            break 
+        n += 1
+        if n > 2: 
+            n=0
+
+
+    
+
+
 
